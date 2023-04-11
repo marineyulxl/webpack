92ccc0d170b7dff099c46064574ba8d698ee558a
marineyu <1835741315@qq.com>
2023-04-11 19:31:19 +0800

    webpack_entry

---
+++ demo_01/src/index.js
@@ -0,0 +1,6 @@
+import m1 from './m.js'
+console.log('你好');
+// setTimeout(()=>{
+//     console.log('不好');
+// })
+m1.sayH()
---
+++ demo_01/src/m.js
@@ -0,0 +1,6 @@
+import $ from 'jquery'
+export default {
+    sayH(){
+        $('body').append('<h1>你好呀</h1>')
+    }
+}
